4e042fbd73a8aa8a18f8efa190ff6409125d01e6
alexandrNast <nastyushenko@softlab.ru>
2023-12-08 12:06:31 +0700

    Initial commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,6 +6,17 @@ import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
+        int[] arr = {3, 2, 2, 2, 8, 10};
+        int sum = 5;
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            int complement = sum - arr[i];
+            if (map.containsKey(complement)) {
+                System.out.println(arr[i] + ", " + complement);
+            }
+            map.put(arr[i], i);
+        }
 //        Meeting meeting1 = new Meeting(10, 20);
 //        Meeting meeting2 = new Meeting(15, 25);
 //        System.out.println("Пересекаются ли встречи: " + meeting1.isOverlap(meeting2));
@@ -29,25 +40,25 @@ public class Main {
 //                .collect(Collectors.toList());
 //    }
 
-        HashMap<Integer, String> passportsAndNames = new HashMap<>();
-        HashMap<Integer, String> passportsAndNames2 = new HashMap<>();
-
-        passportsAndNames.put(212133, "Лидия Аркадьевна Бубликова");
-        passportsAndNames.put(162348, "Иван Михайлович Серебряков");
-        passportsAndNames.put(8082771, "Дональд Джон Трамп");
-
-        passportsAndNames2.put(917352, "Алексей Андреевич Ермаков");
-        passportsAndNames2.put(925648, "Максим Олегович Архаров");
-
-
-        passportsAndNames.putAll(passportsAndNames2);
-//        System.out.println(passportsAndNames);
-
-        for (Map.Entry entry : passportsAndNames.entrySet()) {
-
-            System.out.println(entry.getKey());
-
-        }
+//        HashMap<Integer, String> passportsAndNames = new HashMap<>();
+//        HashMap<Integer, String> passportsAndNames2 = new HashMap<>();
+//
+//        passportsAndNames.put(212133, "Лидия Аркадьевна Бубликова");
+//        passportsAndNames.put(162348, "Иван Михайлович Серебряков");
+//        passportsAndNames.put(8082771, "Дональд Джон Трамп");
+//
+//        passportsAndNames2.put(917352, "Алексей Андреевич Ермаков");
+//        passportsAndNames2.put(925648, "Максим Олегович Архаров");
+//
+//
+//        passportsAndNames.putAll(passportsAndNames2);
+////        System.out.println(passportsAndNames);
+//
+//        for (Map.Entry entry : passportsAndNames.entrySet()) {
+//
+//            System.out.println(entry.getKey());
+//
+//        }
 
     }
 }
